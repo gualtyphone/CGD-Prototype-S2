a4c4c6d9504e255a450ac8f637ecf13c4647eea1
gualtyphone <gualtyphone@me.com>
2018-01-17 05:03:31 +0000

    1st commit, tests with ragdolls and characters

---
+++ Assets/Follow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    [SerializeField]
+    GameObject objectToFollow;
+
+    [SerializeField]
+    Vector3 offset;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = objectToFollow.transform.position + offset;
+        transform.LookAt(objectToFollow.transform);
+            
+    }
+}
---
+++ Assets/Scripts/RagdollController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Effect
+{
+    public Rigidbody ragdollPart;
+    public Vector3 force;
+    public KeyCode input;
+
+}
+
+public class RagdollController : MonoBehaviour {
+
+    public Effect[] effects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        foreach (Effect e in effects)
+        {
+            if (Input.GetKey(e.input))
+            {
+                e.ragdollPart.AddRelativeForce(e.force);
+            }
+        }
+	}
+}
